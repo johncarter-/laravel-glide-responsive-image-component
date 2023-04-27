e870be950fc7830d2cc3979c94a7eb9407e2a2f2
John Carter <john@johncarter.co.uk>
2023-04-27 08:56:26 +0100

    Add Livewire support on change

--- resources/views/components/responsive-image.blade.php
+++ resources/views/components/responsive-image.blade.php
@@ -1,19 +1,34 @@
 @once
   <script>
     window.addEventListener('load', function() {
-      window.responsiveResizeObserver = new ResizeObserver((entries) => {
-        entries.forEach(entry => {
-          const imgWidth = entry.target.getBoundingClientRect().width;
-          entry.target.parentNode.querySelectorAll('source').forEach((source) => {
-            source.sizes = Math.ceil(imgWidth / window.innerWidth * 100) + 'vw';
-          });
-        });
-      });
-      document.querySelectorAll('[data-laravel-glide-responsive-image-component]').forEach(
-        responsiveImage => {
-          responsiveResizeObserver.onload = null;
-          responsiveResizeObserver.observe(responsiveImage);
-        });
+
+        const resizeObserverCallback = (entries, observer) => {
+            entries.forEach(entry => {
+                const imgWidth = entry.target.getBoundingClientRect().width;
+                entry.target.parentNode.querySelectorAll('source').forEach(source => {
+                    source.sizes = `${Math.ceil(imgWidth / window.innerWidth * 100)}vw`;
+                });
+            });
+        };
+
+        const triggerResponsiveImageUpdate = () => {
+            const responsiveImages = document.querySelectorAll('[data-laravel-glide-responsive-image-component]');
+            if (responsiveImages.length > 0) {
+                window.responsiveResizeObserver.disconnect(); // Disconnect observer to prevent duplicate observations
+                responsiveImages.forEach(responsiveImage => {
+                    window.responsiveResizeObserver.observe(responsiveImage);
+                });
+            }
+        };
+
+        window.responsiveResizeObserver = new ResizeObserver(resizeObserverCallback);
+
+        triggerResponsiveImageUpdate();
+        
+        if (typeof Livewire !== 'undefined' && Livewire.hook) {
+            Livewire.hook('message.processed', triggerResponsiveImageUpdate);
+        }
+        
     });
   </script>
 @endonce
